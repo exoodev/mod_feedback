3e4b3686496ed9fd11c7b96420c728a8fc6c3e57
tibrus <ibtcom@gmail.com>
2015-06-04 20:16:44 +0300

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -33,7 +33,6 @@ JHtml::_('behavior.formvalidator');
 	</div>
 
 	<div class="uk-form-row">
-		<!-- <label class="uk-form-label" for="form-h-message">Сообщение</label> -->
 		<div class="uk-form-controls">
 			<textarea class="uk-width-1-1 uk-form-large" name="feedback[message]" id="form-h-message" cols="50" rows="5" placeholder="Сообщение"></textarea>
 		</div>
@@ -43,14 +42,11 @@ JHtml::_('behavior.formvalidator');
 		<button class="uk-button uk-button-primary uk-button-large" type="submit">Отправить</button>
 	</div>
 
-	<!-- <div class="g-recaptcha" data-sitekey="6LcFGwcTAAAAAD6obbC86dZ4rqvZkSlwY2EvfpvO"></div> -->
-
 	<input name="option" value="com_ajax" hidden>
 	<input name="module" value="feedback" hidden>
 	<input name="format" value="raw" hidden>
 
 	<?php echo JHtml::_( 'form.token' ); ?>
-
 </form>
 
 </div>
